12201014924d1f95bdf7a814b391aa5a8368f970
Daniel Cloud <daniel@danielcloud.org>
2016-02-01 17:31:21 -0500

    Move debug_toolbar out of regular installed apps

--- newsilkroad/settings.py
+++ newsilkroad/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'debug_toolbar',
     'django_extensions',
 
     'markymark',
@@ -42,6 +41,9 @@ INSTALLED_APPS = [
 
     'facts',
 ]
+
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
