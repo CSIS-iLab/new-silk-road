1434ddb4a64a3c33c23b05404126a15a73fdfabd
Daniel Cloud <daniel@danielcloud.org>
2016-02-09 15:58:52 -0500

    Add view skeletons for category stuff

--- knowledgebase/urls.py
+++ knowledgebase/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from knowledgebase.views import ArticleDetailView
+from knowledgebase.views import ArticleDetailView, CategoryListView, CategoryDetailView
 
 urlpatterns = [
+    url(r'^categories/$', CategoryListView.as_view(), name='category-list'),
+    url(r'^categories/(?P<slug>[-\w]+)/$', CategoryDetailView.as_view(), name='category-detail'),
     url(r'^(?P<slug>[-\w]+)/$', ArticleDetailView.as_view(), name='article-detail'),
 ]
--- knowledgebase/views.py
+++ knowledgebase/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-from knowledgebase.models import Article
+from knowledgebase.models import Article, Category
 
 
 class ArticleDetailView(DetailView):
     model = Article
+
+
+class CategoryListView(ListView):
+    model = Category
+
+
+class CategoryDetailView(DetailView):
+    model = Category
