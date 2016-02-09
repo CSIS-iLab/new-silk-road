3b805609ff41c7b976b6a75ceb3129fafa9799f4
Daniel Cloud <daniel@danielcloud.org>
2016-02-09 14:25:05 -0500

    Add Category model

--- knowledgebase/models.py
+++ knowledgebase/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from django.utils.text import slugify
+from mptt.models import MPTTModel, TreeForeignKey
 from markymark.fields import MarkdownField
 from markymark.utils import render_markdown
-from django.utils.text import slugify
-
 from taggit.managers import TaggableManager
 
 
@@ -11,9 +11,9 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = MarkdownField()
-    body_rendered = models.TextField(blank=True)
+    body_rendered = models.TextField(blank=True, editable=False)
 
-    tags = TaggableManager()
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.slug
@@ -30,3 +30,18 @@ class Article(models.Model):
         if not self.slug or self.slug == '':
             self._set_slug()
         super(Article, self).save(*args, **kwargs)
+
+
+class Category(MPTTModel):
+    """An Article's Category"""
+    name = models.CharField(max_length=100, unique=True)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
