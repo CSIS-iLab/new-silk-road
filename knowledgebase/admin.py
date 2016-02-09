a7737fd396a45555ae4cc74354e7d4249c86b3e6
Daniel Cloud <daniel@danielcloud.org>
2016-02-09 14:26:59 -0500

    Add admin for knowledgebase models. Custom widget for field when editing body markdown.

--- knowledgebase/admin.py
+++ knowledgebase/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from knowledgebase.models import Article, Category
+from markymark.fields import MarkdownField
+from knowledgebase.widgets import BigMarkdownTextarea
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    formfield_overrides = {
+        MarkdownField: {'widget': BigMarkdownTextarea}
+    }
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, MPTTModelAdmin)
