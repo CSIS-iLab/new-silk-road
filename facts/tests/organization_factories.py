6609069898d95dcb5de8cd62ae9808b2b150eba1
Daniel Cloud <daniel@danielcloud.org>
2016-03-02 15:09:19 -0500

    Create factories for other organizations, relations. Add dynamic test generation for relations.

--- facts/tests/organization_factories.py
+++ facts/tests/organization_factories.py
@@ -1,4 +1,5 @@
 import factory
+from facts.models.organizations import OrganizationRelationBase
 
 
 class OrganizationFactory(factory.django.DjangoModelFactory):
@@ -29,3 +30,79 @@ class FinancingOrganizationFactory(factory.django.DjangoModelFactory):
 class CompanyStructureFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'facts.CompanyStructure'
+
+
+class CompanyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'facts.Company'
+
+
+class MultilateralFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'facts.Multilateral'
+
+
+class NGOFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'facts.NGO'
+
+
+class PoliticalFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'facts.Political'
+
+
+class MilitaryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'facts.Military'
+
+
+# Relations
+
+class OrganizationRelationBaseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = OrganizationRelationBase
+        abstract = True
+    right = factory.SubFactory(OrganizationFactory)
+
+
+class CompanyRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.CompanyRelation'
+    left = factory.SubFactory(CompanyFactory)
+
+
+class FinancingRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.FinancingRelation'
+    left = factory.SubFactory(FinancingOrganizationFactory)
+
+
+class GovernmentRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.GovernmentRelation'
+    left = factory.SubFactory(GovernmentFactory)
+
+
+class MilitaryRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.MilitaryRelation'
+    left = factory.SubFactory(MilitaryFactory)
+
+
+class MultilateralRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.MultilateralRelation'
+    left = factory.SubFactory(MultilateralFactory)
+
+
+class NGORelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.NGORelation'
+    left = factory.SubFactory(NGOFactory)
+
+
+class PoliticalRelationFactory(OrganizationRelationBaseFactory):
+    class Meta:
+        model = 'facts.PoliticalRelation'
+    left = factory.SubFactory(PoliticalFactory)
