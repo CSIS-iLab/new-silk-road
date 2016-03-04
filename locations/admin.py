7d8d6d43291f6993282f47f3739b5d5e82398051
Daniel Cloud <daniel@danielcloud.org>
2016-03-04 10:37:35 -0500

    Add CountryChoiceField to Region's admin

--- locations/admin.py
+++ locations/admin.py
@@ -1,8 +1,22 @@
 from django.contrib.gis import admin
+from django import forms
 from .models import Region, Place, GeoPoint, GeoRegion
+from .fields import CountryChoiceField
+
+
+class RegionForm(forms.ModelForm):
+    countries = CountryChoiceField(required=False)
+
+    class Meta:
+        model = Region
+        fields = '__all__'
+
+
+class RegionAdmin(admin.OSMGeoAdmin):
+    form = RegionForm
 
 
 admin.site.register(GeoPoint, admin.OSMGeoAdmin)
 admin.site.register(GeoRegion, admin.OSMGeoAdmin)
 admin.site.register(Place, admin.OSMGeoAdmin)
-admin.site.register(Region, admin.OSMGeoAdmin)
+admin.site.register(Region, RegionAdmin)
