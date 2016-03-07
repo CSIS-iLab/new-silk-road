dcaa2eefb1c17789e31ea0455f39c4f148f6ec62
Daniel Cloud <daniel@danielcloud.org>
2016-03-07 16:32:53 -0500

    Customize GeoAdmin. save_on_top helpful when map widgets take up so much space. Show some statistical info for a GeoRegion.

--- locations/admin.py
+++ locations/admin.py
@@ -17,7 +17,27 @@ class RegionAdmin(LeafletGeoAdmin):
     form = RegionForm
 
 
-admin.site.register(GeoPoint, LeafletGeoAdmin)
-admin.site.register(GeoRegion, LeafletGeoAdmin)
+class GeoRegionAdmin(LeafletGeoAdmin):
+    model = GeoRegion
+    list_display = ('label', 'num_points', 'num_geom')
+    save_on_top = True
+
+    def num_points(self, obj):
+        return obj.shape.num_points if obj.shape else 0
+    num_points.short_description = 'Number of Points'
+
+    def num_geom(self, obj):
+        return obj.shape.num_geom if obj.shape else 0
+    num_geom.short_description = 'Number of Shapes'
+
+
+class GeoPointAdmin(LeafletGeoAdmin):
+    model = GeoPoint
+    list_display = ('label', 'lat', 'lon')
+    save_on_top = True
+
+
+admin.site.register(GeoPoint, GeoPointAdmin)
+admin.site.register(GeoRegion, GeoRegionAdmin)
 admin.site.register(Place, LeafletGeoAdmin)
 admin.site.register(Region, RegionAdmin)
