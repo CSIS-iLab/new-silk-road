964c2759ec9275de5812ebe362ff25c14f6e2022
Daniel Cloud <daniel@danielcloud.org>
2016-03-10 16:39:06 -0500

    Add a model for EventType

--- facts/models/events.py
+++ facts/models/events.py
@@ -1,11 +1,21 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 from publish.models import Publishable
 from markymark.fields import MarkdownField
+
+
+class EventType(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children', db_index=True)
 
 
 class Event(Publishable):
     """Describes an event, one which may have a start and end date"""
     name = models.CharField(max_length=100)
+    event_type = models.ForeignKey('EventType', verbose_name='type',
+                                   on_delete=models.SET_NULL,
+                                   blank=True, null=True)
     description = MarkdownField(blank=True)
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True)
