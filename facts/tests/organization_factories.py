cc92a32bd29c5cf4ba179d0ad29056c6d5d77bea
Daniel Cloud <daniel@danielcloud.org>
2016-03-23 12:35:19 -0400

    More work setting up/updating test factories

--- facts/tests/organization_factories.py
+++ facts/tests/organization_factories.py
@@ -6,6 +6,39 @@ class OrganizationFactory(factory.django.DjangoModelFactory):
         model = 'facts.Organization'
 
     name = factory.Sequence(lambda n: 'Test Organization%s' % n)
+    mission = factory.Faker('paragraph')
+    # leaders = factory.SubFactory(PersonFactory)
+    notes = factory.Faker('paragraph')
+    # headquarters = factory.SubFactory(PlaceFactory)
+    founding_date = factory.Faker('date')
+    dissolution_date = factory.Faker('date')
+
+    @factory.post_generation
+    def related_organizations(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for obj in extracted:
+                self.related_organizations.add(obj)
+
+    @factory.post_generation
+    def initiatives(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for obj in extracted:
+                self.initiatives.add(obj)
+
+    @factory.post_generation
+    def events(self, create, extracted, **kwargs):
+        if not create:
+            return
+
+        if extracted:
+            for obj in extracted:
+                self.events.add(obj)
 
 
 class SubOrganizationFactory(OrganizationFactory):
@@ -16,45 +49,73 @@ class SubOrganizationFactory(OrganizationFactory):
     parent = factory.SubFactory(OrganizationFactory)
 
 
-class OrganizationDetailFactory(factory.django.DjangoModelFactory):
+class DetailsFactory(factory.django.DjangoModelFactory):
     organization = factory.SubFactory(OrganizationFactory)
 
 
-class GovernmentDetailsFactory(OrganizationDetailFactory):
-    class Meta:
-        model = 'facts.GovernmentDetails'
-
-
-class FinancingOrganizationDetailsFactory(OrganizationDetailFactory):
-    class Meta:
-        model = 'facts.FinancingOrganizationDetails'
-
-
-class CompanyStructureDetailsFactory(OrganizationDetailFactory):
+class CompanyStructureFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'facts.CompanyStructure'
 
 
-class CompanyDetailsFactory(OrganizationDetailFactory):
+class CompanyDetailsFactory(DetailsFactory):
     class Meta:
         model = 'facts.CompanyDetails'
 
+    structure = factory.SubFactory(CompanyStructureFactory)
+    sector = factory.Iterator(range(1, 4))
+    # org_type =
 
-class MultilateralDetailsFactory(OrganizationDetailFactory):
+
+class FinancingOrganizationDetailsFactory(DetailsFactory):
+    class Meta:
+        model = 'facts.FinancingOrganizationDetails'
+
+    # approved_capital
+    # moodys_credit_rating
+    # fitch_credit_rating
+    # sp_credit_rating
+    # shareholder_people
+    # scope_of_operations
+    # procurement
+    # org_type
+
+
+class GovernmentDetailsFactory(DetailsFactory):
+    class Meta:
+        model = 'facts.GovernmentDetails'
+
+
+class MilitaryDetailsFactory(DetailsFactory):
+    class Meta:
+        model = 'facts.MilitaryDetails'
+
+
+class MultilateralDetailsFactory(DetailsFactory):
     class Meta:
         model = 'facts.MultilateralDetails'
 
 
-class NGODetailsFactory(OrganizationDetailFactory):
+class NGODetailsFactory(DetailsFactory):
     class Meta:
         model = 'facts.NGODetails'
 
 
-class PoliticalDetailsFactory(OrganizationDetailFactory):
+class PoliticalDetailsFactory(DetailsFactory):
     class Meta:
         model = 'facts.PoliticalDetails'
 
 
-class MilitaryDetailsFactory(OrganizationDetailFactory):
+class OrganizationShareholderFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'facts.MilitaryDetails'
+        model = 'facts.OrganizationShareholder'
+    investment = factory.SubFactory(OrganizationFactory)
+    shareholder = factory.SubFactory(FinancingOrganizationDetailsFactory)
+    # value = Decimal
+
+
+class FODetailsWithShareholdersFactory(FinancingOrganizationDetailsFactory):
+    shareholder_organizations = factory.RelatedFactory(
+        OrganizationShareholderFactory,
+        'shareholder'
+    )
