72e37ff2e0a4cdf5a728347dd827aef34b39cae0
Daniel Cloud <daniel@danielcloud.org>
2016-03-25 14:42:50 -0400

    Force CURRENCY_CHOICES to have a determined order so we don't remake migrations again

--- finance/currency.py
+++ finance/currency.py
@@ -1,6 +1,10 @@
 from iso4217 import Currency
 
 
-CURRENCY_CHOICES = tuple((c.code, c.currency_name) for c in Currency)
+def make_currency_choices():
+    currencies = ((c.code, c.currency_name) for c in Currency)
+    return tuple(sorted(currencies, key=lambda x: x[1]))
+
+CURRENCY_CHOICES = make_currency_choices()
 
 DEFAULT_CURRENCY_CHOICE = Currency.usd.code
