448a53b8630aa0c92705f973806fe2bf48dd3474
Daniel Cloud <daniel@danielcloud.org>
2016-03-28 11:55:00 -0400

    Increase max_length of a sources.Document url to 500 char

--- sources/models.py
+++ sources/models.py
@@ -6,7 +6,7 @@ from filer.fields.file import FilerFileField
 class Document(Temporal):
     """A Document"""
     source_file = FilerFileField(verbose_name='File')
-    url = models.URLField('URL', blank=True)
+    url = models.URLField('URL', blank=True, max_length=500)
 
     def __str__(self):
         return self.name
