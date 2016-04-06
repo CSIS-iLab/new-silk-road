3c38a87c30d0964ef5fe46f55b6a101c91d92f28
Daniel Cloud <daniel@danielcloud.org>
2016-04-06 12:58:03 -0400

    Make some display stuff for admin

--- locations/admin.py
+++ locations/admin.py
@@ -14,8 +14,16 @@ class MapAdmin(LeafletGeoAdmin):
     save_on_top = True
 
 
-class LocationGeoAdmin(MapAdmin):
+class GeometryBaseAdmin(MapAdmin):
     readonly_fields = ('attributes',)
+
+    def num_points(self, obj):
+        return obj.geometry.num_points if obj.geometry else 0
+    num_points.short_description = 'Number of Points'
+
+    def num_geom(self, obj):
+        return obj.geometry.num_geom if obj.geometry else 0
+    num_geom.short_description = 'Number of Shapes'
 
 
 class RegionForm(forms.ModelForm):
@@ -32,34 +40,38 @@ class RegionAdmin(MapAdmin):
 
 
 @admin.register(PolygonGeometry)
-class PolygonGeometryAdmin(LocationGeoAdmin):
+class PolygonGeometryAdmin(GeometryBaseAdmin):
     model = PolygonGeometry
     list_display = ('label', 'num_points', 'num_geom')
 
-    def num_points(self, obj):
-        return obj.geometry.num_points if obj.geometry else 0
-    num_points.short_description = 'Number of Points'
-
-    def num_geom(self, obj):
-        return obj.geometry.num_geom if obj.geometry else 0
-    num_geom.short_description = 'Number of Shapes'
-
 
 @admin.register(PointGeometry)
-class PointGeometryAdmin(LocationGeoAdmin):
+class PointGeometryAdmin(GeometryBaseAdmin):
     model = PointGeometry
-    list_display = ('label',)
+    list_display = ('label', 'latitude', 'longitude')
+    readonly_fields = ('latitude', 'longitude', 'attributes')
 
 
 @admin.register(LineStringGeometry)
-class LineStringGeometryAdmin(LocationGeoAdmin):
+class LineStringGeometryAdmin(GeometryBaseAdmin):
     model = LineStringGeometry
-    list_display = ('label',)
+    list_display = ('__str__', 'label', 'num_points', 'num_geom',)
 
 
 @admin.register(GeometryCollection)
 class GeometryCollectionAdmin(admin.ModelAdmin):
     model = GeometryCollection
     save_on_top = True
+    readonly_fields = ('attributes', 'geometry_count', 'geometry_type_candidate')
+    list_display = ('label', 'geometry_count', 'geometry_type_candidate',)
+
+    def geometry_count(self, obj):
+        return obj.geometryrecord_set.count()
+
+    def geometry_type_candidate(self, obj):
+        record = obj.geometryrecord_set.first()
+        geom = record.get_geometry()
+        return geom.geom_type if geom else None
+    geometry_type_candidate.short_description = 'Geometry Type (estimated)'
 
 admin.site.register(Place, LeafletGeoAdmin)
