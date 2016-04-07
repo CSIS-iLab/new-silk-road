d9b13e0f8b21b249b1efad39996862da2bf08304
Daniel Cloud <daniel@danielcloud.org>
2016-04-07 15:53:09 -0400

    Add map page to website urls

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from .views import HomeView
+from infrastructure.views import ProjectsMapView
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='website-home'),
+    url(r'^map/$', ProjectsMapView.as_view(), name='website-map'),
 ]
