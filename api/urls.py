028506979e0ce73c4ce256b1aa290a60e003da81
Daniel Cloud <daniel@danielcloud.org>
2016-04-13 18:25:50 -0400

    Flatten API urls

--- api/urls.py
+++ api/urls.py
@@ -16,15 +16,12 @@ from api.views import (
 router = routers.DefaultRouter()
 router.register(r'projects', ProjectViewSet)
 router.register(r'initiatives', InitiativeViewSet)
-
-geo_router = routers.DefaultRouter()
-geo_router.register(r'lines', LineStringGeometryViewSet)
-geo_router.register(r'points', PointGeometryViewSet)
-geo_router.register(r'polygons', PolygonGeometryViewSet)
-geo_router.register(r'store', GeometryStoreViewSet)
+router.register(r'lines', LineStringGeometryViewSet)
+router.register(r'points', PointGeometryViewSet)
+router.register(r'polygons', PolygonGeometryViewSet)
+router.register(r'geostore', GeometryStoreViewSet)
 
 
 urlpatterns = [
-    url(r'^geo/', include(geo_router.urls)),
     url(r'^', include(router.urls)),
 ]
