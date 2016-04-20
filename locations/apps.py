4d739ee1fb20f035ec6390182d31adaa5ff47d9b
Daniel Cloud <daniel@danielcloud.org>
2016-04-20 10:36:14 -0400

    Move update_centroid_for_geostore signal connection logic to apps.py and clean up.

--- locations/apps.py
+++ locations/apps.py
@@ -1,13 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import m2m_changed
 
 
 class LocationsConfig(AppConfig):
     name = 'locations'
 
     def ready(self):
-        # from locations.signals import update_centroid_for_geostore
-        import locations.signals
-        # logger.debug('Locations ready')
-        # request_finished.connect(my_callback)
-        # GeometryStore = self.get_model('GeometryStore')
-        # m2m_changed.connect(update_centroid_for_geostore, sender=GeometryStore.points.through)
+        GeometryStore = self.get_model('GeometryStore')
+        from locations.signals import update_centroid_for_geostore
+        m2m_changed.connect(update_centroid_for_geostore, sender=GeometryStore.lines.through)
+        m2m_changed.connect(update_centroid_for_geostore, sender=GeometryStore.points.through)
+        m2m_changed.connect(update_centroid_for_geostore, sender=GeometryStore.polygons.through)
--- locations/signals.py
+++ locations/signals.py
@@ -1,12 +1,6 @@
-from django.db.models.signals import m2m_changed
-from django.dispatch import receiver
-
 from locations.models import GeometryStore
 
 
-@receiver(m2m_changed, sender=GeometryStore.lines.through)
-@receiver(m2m_changed, sender=GeometryStore.points.through)
-@receiver(m2m_changed, sender=GeometryStore.polygons.through)
 def update_centroid_for_geostore(sender, instance, action, reverse, model, pk_set, using, **kwargs):
     if isinstance(instance, GeometryStore) and action in ('post_add', 'post_remove'):
         coll_centroid = instance._get_collection_centroid()
