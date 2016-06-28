9d277e1f633cf4d666867f58971843050e0351e1
Daniel Cloud <daniel@danielcloud.org>
2016-06-28 14:10:18 -0400

    Update date_extras with a fuzzydate_filter that uses utils.date.fuzzydate and django.utils.dateformat to format fuzzy dates

--- utilities/date.py
+++ utilities/date.py
@@ -1,4 +1,4 @@
-
+from datetime import date
 
 class fuzzydate:
     def __new__(cls, year=None, month=None, day=None):
@@ -20,6 +20,14 @@ class fuzzydate:
     @property
     def day(self):
         return self._day
+
+    def fillin(self, arg):
+        '''
+        Create a date object by filling in missing values from another date object
+        '''
+        if not isinstance(arg, date):
+            raise TypeError('filldate a date object to fill in missing values')
+        return date(self.year or arg.year, self.month or arg.month, self.day or arg.day)
 
     def __str__(self):
         return 'fuzzydate({self.year}, {self.month}, {self.day})'.format(self=self)
--- utilities/templatetags/date_extras.py
+++ utilities/templatetags/date_extras.py
@@ -1,14 +1,31 @@
 from django import template
+from django.utils import dateformat
 from datetime import date
+from utilities.date import fuzzydate
 
 register = template.Library()
 
-MONTH_FORMATS = frozenset(('b', 'B', 'h', 'm'))
+YEAR_FORMATS = 'yYo'
+MONTH_FORMATS = 'bEFmMnN'
+DAY_FORMATS = 'dDjS'
 
 
-@register.filter()
-def month(value, arg):
-    if arg in MONTH_FORMATS and value:
-        fakedate = date(1, value, 1)
-        return fakedate.strftime('%{}'.format(arg))
-    return None
+@register.filter(name='fuzzydate')
+def fuzzydate_filter(value, arg):
+    '''
+    This fuzzydate filter is a formatter for fuzzy dates, with limitations.
+    Currently, you must separate format characters by whitespace. No other separators work.
+    '''
+    def process_format(format_parts, value):
+        for part in format_parts:
+            if all((p in YEAR_FORMATS for p in part)) and value.year:
+                yield part
+            elif all((p in MONTH_FORMATS for p in part)) and value.month:
+                yield part
+            elif all((p in DAY_FORMATS for p in part)) and value.day:
+                yield part
+    if isinstance(value, fuzzydate):
+        format_options = arg.split(' ')
+        format_parts = process_format(format_options, value)
+        fakedate = value.fillin(date.today())
+        return dateformat.format(fakedate, ' '.join(format_parts))
