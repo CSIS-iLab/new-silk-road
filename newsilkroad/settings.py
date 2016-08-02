9395805f3d5c229182d5a4fe23af8be12f7de6be
Daniel Cloud <daniel@danielcloud.org>
2016-08-02 15:00:54 -0400

    Add django-constance for some dynamic settings. Configure constance to handle setting slugs of collections for website crap.

--- newsilkroad/settings.py
+++ newsilkroad/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
 
     'maintenancemode',
     'cachalot',
+    'constance',
+    'constance.backends.database',
 
     'django_extensions',
     'storages',
@@ -296,10 +298,26 @@ SUIT_CONFIG = {
             'website.Collection',
             'website.CollectionItem',
         )},
+        'constance',
         'sites',
         '-',
         'auth',
     )
+}
+
+# Website (app) config
+CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
+CONSTANCE_CONFIG = {
+    'FEATURED_DATABASE_COLLECTION': (
+        'database-featured',
+        'Slug for the collection (of database entries) that are'
+        'featured on the db landing page (and possibly other places)', str
+    ),
+    'FEATURED_ANALYSES_COLLECTION': (
+        'analyses-featured',
+        'Slug for the collection of entries (writings.EntryCollection) that are'
+        'featured on the website', str
+    ),
 }
 
 # markymark
--- website/views.py
+++ website/views.py
@@ -1,8 +1,22 @@
 from django.views.generic import TemplateView
+from constance import config
+from writings.models import EntryCollection
+
+FEATURED_ANALYSES_COLLECTION = getattr(config, 'FEATURED_ANALYSES_COLLECTION', None)
 
 
 class HomeView(TemplateView):
     template_name = "website/home.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs = super().get_context_data(**kwargs)
+        kwargs['featured_analyses'] = None
+        if FEATURED_ANALYSES_COLLECTION:
+            try:
+                kwargs['featured_analyses'] = EntryCollection.objects.get(slug=FEATURED_ANALYSES_COLLECTION)
+            except EntryCollection.DoesNotExist:
+                pass
+        return kwargs
 
 
 class DatabaseView(TemplateView):
