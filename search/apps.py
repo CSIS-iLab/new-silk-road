f520bc56b0c0e049377b3097f39d1593b9dddf78
Daniel Cloud <daniel@danielcloud.org>
2016-08-16 18:23:41 -0400

    Clean up search's appconfig. Remove code to check for first run because it doesn't work and isn't needed since the signal code has dispatch_uids

--- search/apps.py
+++ search/apps.py
@@ -1,22 +1,12 @@
 from django.apps import AppConfig
-import logging
 from search.signals import connect_elasticsearch_signals
-
-logger = logging.getLogger(__name__)
 
 
 class SearchConfig(AppConfig):
     name = 'search'
-    first_run = True
 
     def ready(self):
-        logger.info('Configuring search...')
-
-        if self.first_run:
-
-            from search.mappings import EntryMapping
-            connect_elasticsearch_signals({
-                'writings.Entry': EntryMapping,
-            })
-
-            self.first_run = False
+        from search.mappings import EntryMapping
+        connect_elasticsearch_signals({
+            'writings.Entry': EntryMapping,
+        })
