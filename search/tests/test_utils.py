d37266938daddfdbfe1ebf5c105c9805ba600f4c
Daniel Cloud <daniel@danielcloud.org>
2016-08-17 14:50:54 -0400

    Add tests for create_search_index search util

--- search/tests/test_utils.py
+++ search/tests/test_utils.py
@@ -1,9 +1,11 @@
-from django.test import TestCase
-from search.utils import get_document_class
+from django.test import TestCase, override_settings
+from elasticsearch_dsl.connections import connections
+from search.utils import get_document_class, create_search_index
 from search.documents import ProjectDoc
+from search.tests.settings import TEST_SEARCH
 
 
-class SearchUtilsTestCase(TestCase):
+class GetDocumentClassTestCase(TestCase):
 
     def test_get_document_class_raises_error_on_bad_path(self):
         with self.assertRaises(ImportError):
@@ -18,3 +20,37 @@ class SearchUtilsTestCase(TestCase):
         klass = get_document_class('search.ProjectDoc')
         self.assertIsNotNone(klass)
         self.assertEqual(klass, ProjectDoc)
+
+
+@override_settings(SEARCH=TEST_SEARCH)
+class CreateSearchIndexTestCase(TestCase):
+
+    def setUp(self):
+        connections.create_connection('testing', **TEST_SEARCH['default']['connections'])
+
+    def test_create_search_index_only(self):
+        index = create_search_index('test_create')
+
+        self.assertIsNotNone(index)
+
+        index_dict = index.to_dict()
+        self.assertNotIn('mappings', index_dict)
+
+        index.delete()
+        self.assertFalse(index.exists())
+
+    def test_create_search_index_with_doctypes(self):
+        index = create_search_index('foo', doc_types=('search.ProjectDoc', 'search.EntryDoc'))
+
+        self.assertIsNotNone(index)
+
+        index_dict = index.to_dict()
+        self.assertIn('mappings', index_dict)
+
+        mappings = index_dict.get('mappings')
+        self.assertEqual(len(mappings.keys()), 2)
+        self.assertIn('project_doc', mappings)
+        self.assertIn('entry_doc', mappings)
+
+        index.delete()
+        self.assertFalse(index.exists())
