1469de7333e7bb671578d82590fc6161412b9a77
Daniel Cloud <daniel@danielcloud.org>
2016-08-23 16:16:14 -0400

    Adjust those settings checks in search/__init__.py so I'm not tempted to import the settings values from there.

--- search/__init__.py
+++ search/__init__.py
@@ -7,15 +7,13 @@ default_app_config = 'search.apps.SearchConfig'
 SEARCH_CONFIG = getattr(settings, 'SEARCH', None)
 if not SEARCH_CONFIG:
     raise ImproperlyConfigured('SEARCH settings dictionary must be provided')
-DEFAULT_CONFIG = SEARCH_CONFIG.get('default')
-if not DEFAULT_CONFIG:
+if 'default' not in SEARCH_CONFIG:
     raise ImproperlyConfigured("SEARCH settings must include 'default' settings dict, similar to DATABASES")
-DEFAULT_INDEX = DEFAULT_CONFIG.get('index')
-if not DEFAULT_INDEX:
+if 'index' not in SEARCH_CONFIG['default']:
     raise ImproperlyConfigured("SEARCH settings must include a name for 'index' in 'default' settings dict")
 
 # Set up ES connection
-connection_settings = DEFAULT_CONFIG.get('connections')
+connection_settings = SEARCH_CONFIG['default'].get('connections')
 if not connection_settings:
     raise ImproperlyConfigured("SEARCH settings must include 'connections' dict in 'default'")
 connections.configure(default=connection_settings)
