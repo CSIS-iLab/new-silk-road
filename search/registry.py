dbce4251cedd255984bd8b4e469cf7ab5e35a74a
Daniel Cloud <daniel@danielcloud.org>
2016-08-23 16:10:12 -0400

    Remove index-doctype stuff from SearchRegistry. If we handle Index dynamically, we can do better testing

--- search/registry.py
+++ search/registry.py
@@ -13,7 +13,6 @@ DEFAULT_SERIALIZER_PATH = 'search.serializers'
 class SearchRegistry:
 
     def __init__(self, serializers_path=None):
-        self._doctypes = defaultdict(list)
         self._model_serializers = {}
         self._serializer_module = import_module(serializers_path or DEFAULT_SERIALIZER_PATH)
 
@@ -27,25 +26,14 @@ class SearchRegistry:
             raise TypeError('Invalid model attribute on Meta class for {}'.format(repr(serializer_class)))
         self._model_serializers[model_label] = serializer_class
 
-    def _register_serializer_doctype(self, serializer_class):
-        doc_type = getattr(serializer_class.Meta, 'doc_type', None)
-        if isinstance(doc_type, str):
-            doc_type = get_document_class(doc_type)
-        if doc_type:
-            self._doctypes[doc_type._doc_type.index].append(doc_type)
-
     def _register_serializers(self, serializer_labels):
         for serializer_label in serializer_labels:
             serializer_class = getattr(self._serializer_module, serializer_label, None)
             if serializer_class:
                 self._register_serializer_model(serializer_class)
-                self._register_serializer_doctype(serializer_class)
 
     def register(self, serializer_labels):
         self._register_serializers(serializer_labels)
-
-    def get_doctypes_for_index(self, index_name):
-        return self._doctypes[index_name]
 
     def get_serializer_for_model(self, model):
         if isclass(model) and issubclass(model, models.Model):
--- search/tests/test_registry.py
+++ search/tests/test_registry.py
@@ -1,9 +1,11 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, override_settings
 from unittest.mock import Mock, call
 from search.registry import SearchRegistry
 from search.tests.mocks import MockDocType, MockModel
+from .settings import TEST_SEARCH
 
 
+@override_settings(SEARCH=TEST_SEARCH)
 class SearchRegistryTestCase(SimpleTestCase):
 
     def test_has_serializer_module(self):
@@ -13,14 +15,6 @@ class SearchRegistryTestCase(SimpleTestCase):
 
         serializer_class = getattr(registry._serializer_module, 'MockSerializer', None)
         self.assertIsNotNone(serializer_class)
-
-    def test__register_serializer_doctype_method(self):
-        registry = SearchRegistry('search.tests.mocks')
-        serializer_class = getattr(registry._serializer_module, 'MockSerializer', None)
-        registry._register_serializer_doctype(serializer_class)
-
-        self.assertIn('testindex', registry._doctypes)
-        self.assertIn(MockDocType, registry._doctypes['testindex'])
 
     def test__register_serializer_model_method(self):
         registry = SearchRegistry('search.tests.mocks')
@@ -39,25 +33,6 @@ class SearchRegistryTestCase(SimpleTestCase):
 
         call_list = [call(MockSerializer), call(MockSerializerTwo)]
         registry._register_serializer_model.assert_has_calls(call_list)
-
-    def test_register_method_calls__register_serializer_doctype(self):
-        registry = SearchRegistry('search.tests.mocks')
-        registry._register_serializer_doctype = Mock()
-
-        registry.register(('MockSerializer', 'MockSerializerTwo'))
-        MockSerializer = getattr(registry._serializer_module, 'MockSerializer', None)
-        MockSerializerTwo = getattr(registry._serializer_module, 'MockSerializerTwo', None)
-
-        call_list = [call(MockSerializer), call(MockSerializerTwo)]
-        registry._register_serializer_doctype.assert_has_calls(call_list)
-
-    def test_doctypes_for_index_method(self):
-        registry = SearchRegistry('search.tests.mocks')
-        registry.register(('MockSerializer',))
-        doc_types = registry.get_doctypes_for_index('testindex')
-
-        self.assertIn(MockDocType, doc_types)
-        self.assertEqual(len(doc_types), 1)
 
     def test_get_serializer_for_model_method_with_label(self):
         registry = SearchRegistry('search.tests.mocks')
