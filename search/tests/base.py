6d1fc8f4898ec01c53743d033538326723903f88
Daniel Cloud <daniel@danielcloud.org>
2016-08-25 16:09:20 -0400

    Inspect members of search.documents to get the DocTypes whose index should be overridden for tests.

--- search/tests/base.py
+++ search/tests/base.py
@@ -1,9 +1,11 @@
 from django.test import TestCase, override_settings
 from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl import DocType
 import django_rq
 from search.utils import create_search_index
-from search.documents import EntryDoc, ProjectDoc
+from search import documents
 from .settings import TEST_SEARCH
+import inspect
 
 
 @override_settings(SEARCH=TEST_SEARCH)
@@ -17,8 +19,13 @@ class BaseSearchTestCase(TestCase):
         self.index = create_search_index(SEARCH['default']['index'], SEARCH['default']['doc_types'], connection='testing')
 
         # This is needed for test_documents, but has side effects in all running tests
-        self.index.doc_type(EntryDoc)
-        self.index.doc_type(ProjectDoc)
+        doctypes_list = (value for name, value in inspect.getmembers(documents) if not name.startswith('_') and issubclass(value, DocType) and name != DocType.__name__)
+
+        for doctype in doctypes_list:
+            # Remove assigned index
+            doctype._doc_type.index = None
+            # Associate docs with test index
+            self.index.doc_type(doctype)
 
     def tearDown(self):
         self.index.delete()
