437efa9b3c318ae7c5e7f411e55363bd483c00c3
Daniel Cloud <daniel@danielcloud.org>
2016-10-12 17:31:14 -0400

    Update html_text filter to accept a comma-seperated list of tag names to preserve. Defaults to TEXT_TAGS.

--- website/templatetags/html_utils.py
+++ website/templatetags/html_utils.py
@@ -3,9 +3,13 @@ import bleach
 
 register = template.Library()
 
-TEXT_TAGS = ['p', 'a', 'b', 'i', 'em', 'strong']
+TEXT_TAGS = "p,a,b,i,em,strong"
 
 
 @register.filter(name='html_text')
-def html_text(value):
-    return bleach.clean(value, strip=True, tags=TEXT_TAGS)
+def html_text(value, tags=TEXT_TAGS):
+    '''Cleans up HTML, stripping away all but a list of HTML tags to preserve.
+    By default, this list is "p,a,b,i,em,strong", but you may
+    provide a comma-separated list of tags to preserve.
+    '''
+    return bleach.clean(value, strip=True, tags=[t.strip() for t in str(tags).split(',')])
