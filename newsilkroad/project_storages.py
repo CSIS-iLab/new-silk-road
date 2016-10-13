994b758a24e52ddb76bfbbe7563fdaea08fd1b61
Daniel Cloud <daniel@danielcloud.org>
2016-10-13 17:22:02 -0400

    Update django-storages, switch to boto3 backend.

--- newsilkroad/project_storages.py
+++ newsilkroad/project_storages.py
@@ -1,10 +1,10 @@
 from django.conf import settings
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3BotoStorage):
+class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 
-class MediaStorage(S3BotoStorage):
+class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
