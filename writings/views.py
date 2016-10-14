bb0b98409c83605d82b0c71e6a2d86f294dae8de
Daniel Cloud <daniel@danielcloud.org>
2016-10-14 12:41:25 -0400

    Correct queryset filter for EntryCategoryListView. Closes #81.

--- writings/views.py
+++ writings/views.py
@@ -158,7 +158,7 @@ class EntryCategoryListView(EntryListView):
         queryset = super().get_queryset()
         cat_slug = self.kwargs.get('slug', None)
         self.category = get_object_or_404(Category, slug=cat_slug)
-        return queryset.filter(category=self.category)
+        return queryset.filter(categories=self.category)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
