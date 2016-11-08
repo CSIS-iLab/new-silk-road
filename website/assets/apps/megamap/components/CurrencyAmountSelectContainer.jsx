39d7fb7f14cd3da4f716ace1e9a07a0e12b01679
Daniel Cloud <daniel@danielcloud.org>
2016-11-08 14:41:33 -0500

    Update CurrencyAmountSelectContainer and SearchBar syntax to latest react/es6 (with linting).

--- website/assets/apps/megamap/components/CurrencyAmountSelectContainer.jsx
+++ website/assets/apps/megamap/components/CurrencyAmountSelectContainer.jsx
@@ -1,58 +1,65 @@
 import React, { Component, PropTypes } from 'react';
+import Select from 'react-select';
 import CurrencyStore from '../stores/CurrencyStore';
 import CurrencyActions from '../actions/CurrencyActions';
-import Select from 'react-select';
 import Option from '../models/Option';
 
-export default class CurrencyAmountSelectContainer extends Component {
-  static propTypes = {
-    onSelect: PropTypes.func
-  }
+class CurrencyAmountSelectContainer extends Component {
 
-  state = {
-    isLoading: true,
-    options: [],
-    value: '',
-    lookups: {},
-    error: null
-  }
+  static get labelName() { return 'Cost'; }
 
-  get labelName() { return 'Cost'; }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      options: [],
+      value: '',
+      lookups: {},
+      error: null,
+    };
+    this.updateOptions = this.updateOptions.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
 
   componentDidMount() {
     CurrencyStore.listen(this.updateOptions);
     CurrencyActions.fetch();
   }
 
-  updateOptions = (data) => {
-    this.setState({
-      lookups: data.lookups,
-      options: Object.keys(data.lookups).map((key) => new Option(key, key)),
-      error: data.error,
-      isLoading: false
-    });
-  }
-
-  onChange = (option, event) => {
+  onChange(option) {
     const value = option ? option.value : '';
-    this.setState({value});
+    this.setState({ value });
     if (this.props.onSelect) {
       this.props.onSelect(this.state.lookups[value]);
     }
   }
 
+  updateOptions(data) {
+    this.setState({
+      lookups: data.lookups,
+      options: Object.keys(data.lookups).map(key => new Option(key, key)),
+      error: data.error,
+      isLoading: false,
+    });
+  }
+
   render() {
-    const hasOptions = this.state.options.length > 0;
     return (
       <Select
         value={this.state.value}
-        name='cost'
+        name="cost"
         placeholder={this.labelName}
         options={this.state.options}
         onChange={this.onChange}
         isLoading={this.state.isLoading}
-        />
+      />
     );
   }
 
 }
+
+CurrencyAmountSelectContainer.propTypes = {
+  onSelect: PropTypes.func,
+};
+
+export default CurrencyAmountSelectContainer;
--- website/assets/apps/megamap/components/SearchBar.jsx
+++ website/assets/apps/megamap/components/SearchBar.jsx
@@ -1,31 +1,21 @@
-import React, { Component, PropTypes } from "react";
-import Radium, { Style } from "radium";
-import { Input, Button } from "./forms";
+import React, { Component, PropTypes } from 'react';
+import Radium from 'radium';
+import { Input, Button } from './forms';
 
 class SearchBar extends Component {
-  static propTypes = {
-    id: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    placeholder: PropTypes.string,
-    label: PropTypes.string,
-    searchEnabled: PropTypes.bool.isRequired,
-    onSearchInput: PropTypes.func,
-    showSubmit: PropTypes.bool
-  };
 
-  static defaultProps = {
-    showSubmit: true
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+    this.handleUserInput = this.handleUserInput.bind(this);
   }
 
-  state = {
-    inputText: '',
-
-  }
-
-  handleUserInput = (value, e) => {
-    const {name} = this.props;
+  handleUserInput(value) {
+    const { name } = this.props;
     if (this.props.onSearchInput) {
-      this.setState({inputText: value});
+      this.setState({ inputText: value });
       this.props.onSearchInput({ [name]: value.trim() });
     }
   }
@@ -33,36 +23,42 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="searchBar" id={this.props.id}>
-        {(() => {
-          if (this.props.label) {
-            console.log('label');
-            return (
-              <label for={this.props.name}>
-              {this.props.label}:
-              </label>
-            );
-          }
-        })()}
+        {(this.props.label !== undefined && this.props.label !== '') &&
+          <label htmlFor={this.props.name}>
+            {this.props.label}:
+          </label>
+        }
         <Input
-        ref="searchTextInput"
-        inputText={this.state.inputText}
-        onUserInput={this.handleUserInput.bind(this)}
-        name={this.props.name} placeholder={this.props.placeholder}
+          inputText={this.state.inputText}
+          onUserInput={this.handleUserInput}
+          name={this.props.name}
+          placeholder={this.props.placeholder}
         />
-        {(()=> {
-          if (this.props.showSubmit) {
-            return (
-              <Button type='submit'
-              title='Search'
-              enabled={this.props.searchEnabled}
-              >Search</Button>
-            );
-          }
-        })()}
+        {this.props.showSubmit &&
+          <Button
+            type="submit"
+            title="Search"
+            enabled={this.props.searchEnabled}
+          >Search
+          </Button>
+        }
       </div>
-    )
+    );
   }
 }
-SearchBar = Radium(SearchBar);
 
-export default SearchBar;
+SearchBar.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  label: PropTypes.string,
+  searchEnabled: PropTypes.bool.isRequired,
+  onSearchInput: PropTypes.func,
+  showSubmit: PropTypes.bool,
+};
+
+SearchBar.defaultProps = {
+  showSubmit: true,
+};
+
+export default Radium(SearchBar);
