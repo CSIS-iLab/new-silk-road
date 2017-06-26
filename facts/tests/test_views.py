9aa5e81cbba2997a682e9b26b3af4cce31f0967d
Mark Lavin <mlavin@caktusgroup.com>
2017-06-26 16:30:21 -0400

    Testing the person listing.

--- facts/tests/test_views.py
+++ facts/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.test import TestCase
 
 from .person_factories import PersonFactory
@@ -9,7 +10,6 @@ class PersonDetailViewTestCase(TestCase):
     def setUp(self):
         super().setUp()
         self.person = PersonFactory()
-        self.url = self.person.get_absolute_url()
 
     def test_get_person_details(self):
         """Render page details of the given person."""
@@ -28,3 +28,34 @@ class PersonDetailViewTestCase(TestCase):
 
         response = self.client.get(self.person.get_absolute_url())
         self.assertEqual(response.status_code, 404)
+
+
+class PersonListingViewTestCase(TestCase):
+    """Viewing a list of people."""
+
+    def setUp(self):
+        super().setUp()
+        self.person = PersonFactory()
+        self.other = PersonFactory()
+        self.url = reverse('facts:person-list')
+
+    def test_get_listing(self):
+        """Render page people."""
+
+        with self.assertTemplateUsed('facts/person_list.html'):
+            response = self.client.get(self.url)
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, self.person.get_absolute_url())
+            self.assertContains(response, self.other.get_absolute_url())
+
+    def test_get_unpublished_person(self):
+        """"Unpublished people should not be visible in the listing."""
+
+        self.person.published = False
+        self.person.save()
+
+        with self.assertTemplateUsed('facts/person_list.html'):
+            response = self.client.get(self.url)
+            self.assertEqual(response.status_code, 200)
+            self.assertNotContains(response, self.person.get_absolute_url())
+            self.assertContains(response, self.other.get_absolute_url())
