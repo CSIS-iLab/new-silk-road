5f16b61b55337309283a2ebb9d472b8e15a9ccb7
Mark Lavin <mlavin@caktusgroup.com>
2017-07-26 14:24:46 -0400

    Additional coverage for date utility edge cases.

--- utilities/tests.py
+++ utilities/tests.py
@@ -1,7 +1,9 @@
-from django.test import SimpleTestCase
-from utilities.templatetags.date_extras import fuzzydate_filter
-from utilities.date import fuzzydate
 from datetime import date
+
+from django.test import SimpleTestCase
+
+from utilities.date import fuzzydate
+from utilities.templatetags.date_extras import fuzzydate_filter
 
 
 class FuzzyDateTestCase(SimpleTestCase):
@@ -25,6 +27,12 @@ class FuzzyDateTestCase(SimpleTestCase):
 
         d2 = fuzzydate(day=32)
         self.assertIsNone(d2.day)
+
+        d3 = fuzzydate(month=1, day=32)
+        self.assertIsNone(d3.day)
+
+        d4 = fuzzydate(month=1, day=32, year=2000)
+        self.assertIsNone(d4.day)
 
     def test_fuzzydate_can_raise_error_on_invalid_month(self):
         with self.assertRaises(ValueError):
@@ -55,12 +63,22 @@ class FuzzyDateTestCase(SimpleTestCase):
         self.assertEqual(id4_date.day, 4)
 
     def test_fuzzydate_can_raise_error_when_date_would_be_invalid(self):
-        '''fuzzydate can raise an error on fuzzydate(2015, 2, 29, raise_errors=True) because Feb 29, 2015 was not a leap year.'''
+        """fuzzydate can raise an error on fuzzydate(2015, 2, 29, raise_errors=True)
+
+        2015 was not a leap year."""
+
         with self.assertRaises(ValueError):
             fuzzydate(2015, 2, 29, raise_errors=True)
 
+        fd = fuzzydate(2015, 2, 29, raise_errors=False)
+        self.assertIsNone(fd.day)
+
+        # 2016 was a leap year and shouldn't generate an error
+        fuzzydate(2016, 2, 29, raise_errors=True)
+
     def test_fuzzydate_fillin_raises_error_when_date_would_be_invalid(self):
-        '''fuzzydate.fillin should raise an error when the fillin would create an invalid date.'''
+        """fuzzydate.fillin should raise an error when the fillin would create an invalid date."""
+
         fd = fuzzydate(month=2, day=29)
         fillin_date = date(2015, 7, 4)
         with self.assertRaises(ValueError):
@@ -74,6 +92,15 @@ class FuzzyDateTestCase(SimpleTestCase):
         with self.assertRaises(ValueError):
             fuzzydate(2001, 1, 98, raise_errors=True)
 
+    def test_fuzzydate_fillin_error_on_invalid_arg(self):
+        """Value given to fillin must be a date."""
+
+        fd = fuzzydate(month=2, day=29)
+        for invalid in (1, 'XXX', None, 1.1):
+            with self.subTest('Invalid fillin: {}'.format(invalid)):
+                with self.assertRaises(TypeError):
+                    fd.fillin(invalid)
+
     def test_fuzzydate_sets_invalid_month_to_none(self):
         fd = fuzzydate(2001, 13, 1)
         self.assertIsNone(fd.month)
@@ -86,6 +113,44 @@ class FuzzyDateTestCase(SimpleTestCase):
         fd = fuzzydate(2001, 32, 86)
         self.assertIsNone(fd.month)
         self.assertIsNone(fd.day)
+
+    def test_fuzzydate_equality(self):
+        """Fuzzy dates are equal if all set values are equal."""
+
+        with self.subTest('Day'):
+            self.assertEqual(fuzzydate(day=10), fuzzydate(day=10))
+            self.assertNotEqual(fuzzydate(day=10), fuzzydate(day=1))
+            self.assertNotEqual(fuzzydate(month=1, day=10), fuzzydate(day=10))
+
+        with self.subTest('Month'):
+            self.assertEqual(fuzzydate(month=10), fuzzydate(month=10))
+            self.assertNotEqual(fuzzydate(month=10), fuzzydate(month=1))
+            self.assertNotEqual(fuzzydate(month=1, day=10), fuzzydate(month=1))
+            self.assertEqual(fuzzydate(month=1, day=10), fuzzydate(month=1, day=10))
+
+        with self.subTest('Year'):
+            self.assertEqual(fuzzydate(year=2000), fuzzydate(year=2000))
+            self.assertNotEqual(fuzzydate(year=2000), fuzzydate(year=2001))
+            self.assertNotEqual(fuzzydate(month=1, year=2000), fuzzydate(year=2000))
+            self.assertEqual(fuzzydate(month=1, year=2000), fuzzydate(month=1, year=2000))
+            self.assertNotEqual(fuzzydate(month=1, year=2000), fuzzydate(month=10, year=2000))
+
+        with self.subTest('All'):
+            self.assertEqual(
+                fuzzydate(month=1, day=1, year=2000), fuzzydate(month=1, day=1, year=2000))
+            self.assertNotEqual(
+                fuzzydate(month=2, day=1, year=2000), fuzzydate(month=1, day=1, year=2000))
+            self.assertNotEqual(
+                fuzzydate(month=1, day=2, year=2000), fuzzydate(month=1, day=1, year=2000))
+            self.assertNotEqual(
+                fuzzydate(month=1, day=1, year=2000), fuzzydate(month=1, day=1, year=2001))
+            self.assertNotEqual(
+                fuzzydate(month=1, day=1, year=2000), fuzzydate(month=1, year=2000))
+
+    def test_repr(self):
+        """fuzzydate should have a readable repr."""
+
+        self.assertEqual(repr(fuzzydate(month=1, day=31, year=2000)), 'fuzzydate(2000, 1, 31)')
 
 
 class FuzzyDateFilterTestCase(SimpleTestCase):
@@ -110,3 +175,7 @@ class FuzzyDateFilterTestCase(SimpleTestCase):
     def test_fuzzydate_filter_and_fuzzydate_handle_invalid_dates(self):
         date_str = fuzzydate_filter(fuzzydate(month=2, day=42), 'N d Y')
         self.assertEqual('Feb.', date_str)
+
+    def test_fuzzydate_filter_non_fuzzydate(self):
+        for invalid in (1, 'XXX', None, 1.1, date(2001, 1, 31), fuzzydate(month=2, day=31), ):
+            self.assertIsNone(fuzzydate_filter(invalid, 'N d Y'))
