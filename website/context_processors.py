e5a19ff3c9c9683b96c4af627c76dcd6f1df448c
Dmitriy Chukhin <dchukhin@gmail.com>
2017-08-01 09:28:40 -0400

    analytics context_processor gets settings only when called

--- website/context_processors.py
+++ website/context_processors.py
@@ -1,7 +1,5 @@
 from django.conf import settings
 
-GOOGLE_ANALYTICS_KEY = getattr(settings, 'GOOGLE_ANALYTICS_KEY', '')
-
 
 def analytics(request):
     if settings.DEBUG:
@@ -10,5 +8,5 @@ def analytics(request):
         }
     else:
         return {
-            'GOOGLE_ANALYTICS_KEY': GOOGLE_ANALYTICS_KEY
+            'GOOGLE_ANALYTICS_KEY': getattr(settings, 'GOOGLE_ANALYTICS_KEY', '')
         }
