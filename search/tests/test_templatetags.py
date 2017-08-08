f9238be4f7a00d222ad724a6b437eac369c1d090
Mark Lavin <mlavin@caktusgroup.com>
2017-08-08 11:45:12 -0400

    Testing the cleanhightlight tag.

--- search/tests/test_templatetags.py
+++ search/tests/test_templatetags.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 
-from search.templatetags.search_extras import modify_urlquery, facettitle
+from search.templatetags.search_extras import modify_urlquery, facettitle, cleanhighlight
 
 
 class ModifyURLTestCase(SimpleTestCase):
@@ -74,3 +74,36 @@ class FacetTitleTestCase(SimpleTestCase):
         for value, result in tests:
             with self.subTest(value):
                 self.assertEqual(facettitle(value), result)
+
+
+class CleanHighlightTestCase(SimpleTestCase):
+
+    def test_strip_characters(self):
+        """Remove unwanted characters from the result."""
+
+        tests = (
+            # Input, result
+            ('Search term is <em>here</em>.', 'Search term is <em>here</em>'),
+            ('Search term is <em>here</em>,', 'Search term is <em>here</em>'),
+            ('Search term is <em>here</em>:', 'Search term is <em>here</em>'),
+            ('Search term is <em>here</em>;', 'Search term is <em>here</em>'),
+            ('\'Search term is <em>here</em>\'', 'Search term is <em>here</em>'),
+            ('"Search term is <em>here</em>"', 'Search term is <em>here</em>'),
+        )
+        for value, result in tests:
+            with self.subTest(value):
+                self.assertEqual(cleanhighlight(value), result)
+
+    def test_prefix_ellipsis(self):
+        """If the value begins with a space it should be changed to an ellipsis."""
+
+        self.assertEqual(cleanhighlight(' nevermore'), '&hellip;nevermore')
+
+    def test_suffix_ellipsis(self):
+        """If the value contains a space without a period it should be changed to an ellipsis."""
+
+        self.assertEqual(cleanhighlight('Once upon a time '), 'Once upon a time&hellip;')
+
+    def test_break_up_greater_than(self):
+        """If the value contains &gt; it should be broken up and replaced with spaces."""
+        self.assertEqual(cleanhighlight('Home&gt;Projects&gt;Mine'), 'Projects Mine&hellip;')
