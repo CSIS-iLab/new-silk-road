551e504f795e06f81505db00c59eebe4789ab785
Dmitriy Chukhin <dchukhin@gmail.com>
2018-04-12 11:26:13 -0400

    define queryset for RelatedFilters, as it is now required

--- api/filters/facts.py
+++ api/filters/facts.py
@@ -7,6 +7,7 @@ from facts.models import (
 from locations.models import (
     Country,
 )
+from infrastructure.models import Initiative
 from api.filters.infrastructure import InitiativeFilter
 from api.filters.locations import CountryFilter
 
@@ -21,11 +22,32 @@ class PersonFilter(filters.FilterSet):
 class OrganizationFilter(filters.FilterSet):
     name = filters.AllLookupsFilter(name='name')
     slug = filters.AllLookupsFilter(name='slug')
-    leaders = filters.RelatedFilter(PersonFilter, name='leaders', distinct=True)
-    parent = filters.RelatedFilter('api.filters.facts.OrganizationFilter', name='parent')
-    country = filters.RelatedFilter(CountryFilter, name='countries', distinct=True)
-    countries = filters.ModelMultipleChoiceFilter(queryset=Country.objects.all(), name='countries')
-    principal_initiatives = filters.RelatedFilter(InitiativeFilter, name='principal_initiatives')
+    leaders = filters.RelatedFilter(
+        PersonFilter,
+        queryset=Person.objects.all(),
+        name='leaders',
+        distinct=True
+    )
+    parent = filters.RelatedFilter(
+        'api.filters.facts.OrganizationFilter',
+        queryset=Organization.objects.all(),
+        name='parent'
+    )
+    country = filters.RelatedFilter(
+        CountryFilter,
+        queryset=Country.objects.all(),
+        name='countries',
+        distinct=True
+    )
+    countries = filters.ModelMultipleChoiceFilter(
+        queryset=Country.objects.all(),
+        name='countries'
+    )
+    principal_initiatives = filters.RelatedFilter(
+        InitiativeFilter,
+        queryset=Initiative.objects.all(),
+        name='principal_initiatives'
+    )
 
     class Meta:
         model = Organization
