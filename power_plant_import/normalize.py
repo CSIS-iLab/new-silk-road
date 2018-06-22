f7deada2da0e2ba6be24542c040af21a3be68a97
Sean Harrison <sharrison@caktusgroup.com>
2018-06-22 07:37:17 -0500

    normalize Plant Date Online
    * plant_date_online() handles the Date/Month/Year Online.
    * months dict maps month names to numbers (both abbr and full).
    * __main__ gathers all functions

--- power_plant_import/normalize.py
+++ power_plant_import/normalize.py
@@ -2,12 +2,38 @@
 reduce functions that normalize power_plant_data recordsets
 """
 
+months = {
+    "Jan": 1,
+    "January": 1,
+    "Feb": 2,
+    "February": 2,
+    "Mar": 3,
+    "March": 3,
+    "Apr": 4,
+    "April": 4,
+    "May": 5,
+    "Jun": 6,
+    "June": 6,
+    "Jul": 7,
+    "July": 7,
+    "Aug": 8,
+    "August": 8,
+    "Sep": 9,
+    "September": 9,
+    "Oct": 10,
+    "October": 10,
+    "Nov": 11,
+    "November": 11,
+    "Dec": 12,
+    "December": 12,
+}
 
-def field_names_from_matrix(records, source_variables=None, **params):
+
+def field_names_from_matrix(records, **params):
     """for fields where the field name is variable among sources,
     set the record field based on the source matrix (key mapping by dataset))
     """
-    assert source_variables is not None
+    source_variables = params["source_variables"]
     for record in records:
         dataset = record["Dataset"]
         for key in [
@@ -52,9 +78,9 @@ def field_names_from_matrix(records, source_variables=None, **params):
     return records
 
 
-def region_from_country(records, countries_regions=None, **params):
+def region_from_country(records, **params):
     """use the Countries Regions Lookup to set the region value"""
-    assert countries_regions is not None
+    countries_regions = params["countries_regions"]
     for record in records:
         if record.get("Country") is None or countries_regions.get(record["Country"]) is None:
             record["Region"] = None
@@ -63,12 +89,11 @@ def region_from_country(records, countries_regions=None, **params):
     return records
 
 
-def plant_project_status(records, source_variables=None, status_conversions=None, **params):
+def plant_project_status(records, **params):
     """provide Plant and Project Status according to Source Variables matrix.
     (no Status Conversions at this point – that's a merge step)
     """
-    assert source_variables is not None
-    assert status_conversions is not None
+    source_variables = params["source_variables"]
     for record in records:
         dataset = record["Dataset"]
         status_key = source_variables[dataset]["Project Status"]
@@ -86,12 +111,41 @@ def plant_project_status(records, source_variables=None, status_conversions=None
     return records
 
 
+def plant_date_online(records, **params):
+    """Plant Day/Month/Year Online – uses Date/Month/Year Online when Plant Name = Project Name"""
+    source_variables = params["source_variables"]
+    keys = {
+        "Plant Day Online": "Date Online",
+        "Plant Month Online": "Month Online",
+        "Plant Year Online": "Year Online",
+    }
+    for record in records:
+        dataset = record["Dataset"]
+        for key, source_key in keys.items():
+            if (
+                source_variables[dataset][key] in [None, "NA"]
+                or record.get(source_key) is None
+                or record[source_variables[dataset]["Power Plant Name"]]
+                != record.get(source_variables[dataset].get("Project Name"))
+            ):
+                record[key] = None
+            else:
+                if "Month" in key:
+                    record[key] = months[record[source_key]]
+                else:
+                    record[key] = record[source_key]
+    return records
+
+
 if __name__ == "__main__":
     """assume that we're getting a JSON file and producing a JSON file"""
-    import json, os, re, sys, openpyxl
+    import json, os, re, sys, openpyxl, importlib
     from datetime import datetime
     from collections import OrderedDict
     from . import csv, excel
+
+    this = importlib.import_module("power_plant_import.normalize")
+    functions = [f for f in [eval(f) for f in dir(this) if "__" not in f] if "function" in str(f)]
 
     source_matrix_filename = os.path.abspath(sys.argv[1])
     json_filename = os.path.abspath(sys.argv[2])
@@ -100,11 +154,10 @@ if __name__ == "__main__":
     countries_regions = excel.worksheet_dict(source_matrix["Country-Region Lookup"], "Countries")
     with open(json_filename, "r") as f:
         power_plant_data = json.load(f, object_pairs_hook=OrderedDict)
+
     power_plant_data = csv.reduce_power_plant_data(
         power_plant_data,
-        field_names_from_matrix,
-        region_from_country,
-        plant_project_status,
+        *functions,
         source_variables=source_variables,
         countries_regions=countries_regions,
     )
