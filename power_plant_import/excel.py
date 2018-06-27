40366690b24c254465fa08cb7f45b82aaf68abcc
Sean Harrison <sharrison@caktusgroup.com>
2018-06-27 14:43:08 -0500

    excel.load_csv() to handle .csv data
    The latest source data was provided in .csv rather than Excel format, so we need a csv loader function

--- power_plant_import/excel.py
+++ power_plant_import/excel.py
@@ -1,5 +1,5 @@
 
-import openpyxl, re
+import openpyxl, re, csv, chardet, io
 from collections import OrderedDict
 
 
@@ -17,21 +17,23 @@ def load_workbook_data(filepath, headers=True, break_on_blank=True):
     with each worksheet as a list of OrderedDict records, each row as a record.
     (We're using OrderedDict so that worksheets and columns can be accessed by index if desired.)
     """
+
     def make_record(keys, row):
         record = OrderedDict()
         for i in range(len(row)):
             value = row[i].value
-            if row[i].font.italic==True:
+            if row[i].font.italic == True:
                 value = f"<i>{value}</i>"
             record[str(keys[i]).strip()] = value
         return record
+
     workbook = openpyxl.load_workbook(filepath)
     data = OrderedDict()
     for sheet in workbook._sheets:
         data[sheet.title] = []
-        rows = sheet.rows.__iter__()    # sheet.rows doesn't behave as a normal generator, so force it
-        row = rows.__next__()           # <= this consumes the row
-        if headers==True:
+        rows = sheet.rows.__iter__()  # sheet.rows doesn't behave as a normal generator, so force it
+        row = rows.__next__()  # <= this consumes the row
+        if headers == True:
             keys = [cell.value for cell in row]
         else:
             keys = [excel_key(i) for i in range(len(row))]
@@ -39,15 +41,45 @@ def load_workbook_data(filepath, headers=True, break_on_blank=True):
             data[sheet.title].append(record)
         for row in rows:
             record = make_record(keys, row)
-            if break_on_blank==True and set(record.values()) == {None}:
+            if break_on_blank == True and set(record.values()) == {None}:
                 break
             data[sheet.title].append(record)
     return data
 
+
+def load_csv(filename, dialect="excel", encoding=None, headings=True):
+    """load data from an Excel CSV file (comma delimiter)"""
+    with open(filename, "rb") as f:
+        fd = f.read()
+    if encoding is None:
+        encoding = chardet.detect(fd)["encoding"]
+    txt = fd.decode(encoding)
+    reader = csv.reader(io.StringIO(txt), dialect=dialect)
+    data = []
+    if headings == True:
+        # the first row is the keys
+        keys = reader.__next__()
+    else:
+        # the first row is data, keys are letters
+        row = reader.__next__()
+        keys = [excel_key(i) for i in range(len(row))]
+        d = OrderedDict(
+            **{keys[i].strip(): re.sub(r"\s+", " ", row[i].strip()) for i in range(len(row))}
+        )
+        data.append(d)
+    for row in reader:
+        d = OrderedDict(
+            **{keys[i].strip(): re.sub(r"\s+", " ", row[i].strip()) for i in range(len(row))}
+        )
+        data.append(d)
+    return data
+
+
 def excel_key(index):
     """create a key for index by converting index into a base-26 number, using A-Z as the characters."""
-    X = lambda n: ~n and X((n // 26)-1) + chr(65 + (n % 26)) or ''  # chr(65)=='A'
+    X = lambda n: ~n and X((n // 26) - 1) + chr(65 + (n % 26)) or ""  # chr(65)=='A'
     return X(int(index))
+
 
 def worksheet_dict(worksheet_data, primary_key, merge=True):
     """convert a worksheet_data list into a dict, 
@@ -73,7 +105,7 @@ def worksheet_dict(worksheet_data, primary_key, merge=True):
                     if data[key][attr] is None:
                         data[key][attr] = val
                     elif data[key][attr] != val:
-                        print(f"  CONFLICT: key=\"{attr}\":")
+                        print(f'  CONFLICT: key="{attr}":')
                         print(f"    val1: {data[key][attr]}")
                         print(f"    val2: {val}")
     return data
@@ -82,27 +114,28 @@ def worksheet_dict(worksheet_data, primary_key, merge=True):
 def value_to_float(value):
     """convert a value as given to a float"""
     md = re.search(r"([\d\.,]+)", value or "", flags=re.I)
-    if md is None: 
+    if md is None:
         return
     else:
-        number = float(md.group(0).replace(',',''))
+        number = float(md.group(0).replace(",", ""))
         if "million" in value.lower():
             number *= 1e6
         return number
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     import os, sys, json
+
     with_italics = {}
     filenames = sys.argv[1:]
-    print(len(filenames), 'files')
+    print(len(filenames), "files")
     for filename in filenames:
-        print(filenames.index(filename)+1, filename)
+        print(filenames.index(filename) + 1, filename)
         try:
             wbdata = load_workbook_data(filename)
             worksheet_title = list(wbdata.keys())[0]
             worksheet_data = wbdata[worksheet_title]
-            with open(f"{os.path.splitext(filename)[0]}_{worksheet_title}.json", 'w') as f:
+            with open(f"{os.path.splitext(filename)[0]}_{worksheet_title}.json", "w") as f:
                 f.write(json.dumps(worksheet_data, indent=2))
         except:
-            print(os.path.basename(filename), ':', sys.exc_info()[1])
+            print(os.path.basename(filename), ":", sys.exc_info()[1])
