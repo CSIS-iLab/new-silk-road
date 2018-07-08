0002dc7f3fd060708fbed5a87b980b95ee99627d
Sean Harrison <sah@bookgenesis.com>
2018-07-07 19:19:17 -0500

    report_fields: create a report of the fields that occur in a given .json data file and where they first occur.

---
+++ power_plant_import/report_fields.py
@@ -0,0 +1,23 @@
+
+import sys, json
+from power_plant_import import data
+from power_plant_import.fields import FIELDS
+
+if __name__ == '__main__':
+    ppdata = data.read_json(sys.argv[1])
+    fields = []
+    for key in ppdata:
+        for record in ppdata[key]:
+            for field in [
+                field
+                for field in record
+                if record[field] is not None
+                # and field not in FIELDS
+            ]:
+                if (field) not in fields:
+                    fields.append(field)
+                    print(
+                        fields[-1],
+                        '\t',
+                        (record["Dataset"], record["Power Plant Name"], record["Project Name"]),
+                    )
