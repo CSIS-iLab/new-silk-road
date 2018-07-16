84b59038fd3c819b223cf6427f9c3920d1112bf9
Dmitriy Chukhin <dchukhin@gmail.com>
2018-07-16 10:00:59 -0400

    get or create fuels and fuel categories during import

--- power_plant_import/import_csv_to_database.py
+++ power_plant_import/import_csv_to_database.py
@@ -9,9 +9,25 @@ import sys
 sys.path.append(os.environ['PWD'])
 os.environ["DJANGO_SETTINGS_MODULE"] = "newsilkroad.settings"
 django.setup()
+from infrastructure.models import Fuel, FuelCategory
 
 # Set the logging
 logger = logging.getLogger('power_plant_import')
+
+
+def get_or_create_fuels(row):
+    """Get or create the Fuels and FuelCategories for this row."""
+    object_type = row.get('Type')
+    fuels = []
+    for i in range(1, 8):
+        if row.get('{} Fuel {} Category'.format(object_type, i)):
+            fuel_category_name = row.get('Plant Fuel {} Category'.format(i))
+            fuel_category = FuelCategory.objects.get_or_create(name=fuel_category_name)
+            fuel_name = row.get('{} Fuel {}'.format(object_type, i))
+            fuel = Fuel.objects.get_or_create(name=fuel_name, fuel_category=fuel_category)
+            fuels.append(fuel)
+
+    return fuels
 
 
 def import_csv_to_database(*args, **kwargs):
@@ -34,13 +50,22 @@ def import_csv_to_database(*args, **kwargs):
     with open(filename, 'r') as csv_file:
         reader = csv.DictReader(csv_file)
         for row_number, row in enumerate(reader):
-            # Is this a PowerPlant, or a Project?
-            if row.get('Type') == 'Plant':
-                num_successful_imports += 1
-            elif row.get('Type') == 'Project':
-                num_successful_imports += 1
-            else:
-                error_rows.append(row_number)
+            # Here, row_number begins with 0, but in the spreadsheet, that row is
+            # actually row 2 (row 1 is the header row). Use the perceived_row_number
+            # when giving the user output
+            perceived_row_number = row_number + 2
+
+            # Get the object type. If this is neither a 'Plant' nor 'Project', then
+            # add the row to error_rows, and move on to the next row
+            object_type = row.get('Type')
+            if object_type not in ['Plant', 'Project']:
+                error_rows.append(perceived_row_number)
+                continue
+
+            # Get the Fuel(s) for the row
+            fuels = get_or_create_fuels(row)
+
+            num_successful_imports += 1
 
     # Print the summary to the user
     logger.info(
